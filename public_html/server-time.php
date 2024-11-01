bde4109b14359efab13d246ef52d05ad18eb4264
dexen deVries <dexen.devries@gmail.com>
2024-11-01 03:25:12 +0100

    attempt to disable transfer compression
    doesn't seem to work on this server anyhow

--- public_html/server-time.php
+++ public_html/server-time.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('zlib.output_compression', '0');
 header('Expires: Thu, 19 Nov 1981 08:52:00 GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
 header('Pragma: no-cache');
